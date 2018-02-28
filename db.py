5d514a50eecb31f39e7db3506a563d9f61f68aab
alcatrazproduction <yves.huguenin@free.fr>
2018-02-28 07:54:17 +0100

    mods

--- db.py
+++ db.py
@@ -3,16 +3,43 @@
 import MySQLdb
 
 class db(  ):
-	link = None
+	_link 	= None
+#	TB_Name = "" from parent is table name
 
 	def __init__(self):
-		self.link = MySQLdb.connect("localhost","Chrono","Chrono","Chrono" )
+		self._link = MySQLdb.connect("localhost","Chrono","Chrono","Chrono" )
+		if self._desc == None:	# init table description, one for all instance
+			self._desc = []
+		if self._link != None:
+			try:
+				d = self.getTableDescription()
+				for r in d:
+					self._desc.append([r[0], r[1]])
+			except  Exception as e:
+				print(e)
+				print( self._desc )
+			finally:
+				print( self._desc)
+
 
 	def getDbVersion(self):
-		cursor = self.link.cursor()
-# execute SQL query using execute() method.
-		cursor.execute("SELECT VERSION()")
-# Fetch a single row using fetchone() method.
-		data = cursor.fetchone()
-		cursor.close()
-		return data
+		try:
+			cursor = self._link.cursor()
+			cursor.execute("SELECT VERSION()")
+			data = cursor.fetchone()
+			cursor.close()
+		except:
+			data = ""
+		finally:
+			return data
+
+	def getTableDescription(self,):
+		try:
+			cursor = self._link.cursor()
+			cursor.execute("DESC "+ self.__class__.__name__)
+			data = cursor.fetchall()
+			cursor.close()
+		except:
+			data = ""
+		finally:
+			return data
